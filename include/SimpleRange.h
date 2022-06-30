81391f5cfe25bd5996ebf86b758b096a3fcc4e49
Dave Marsh <davemarsh77@gmail.com>
2022-06-30 08:49:46 -0400

    Ordered to Simple

--- include/SimpleRange.h
+++ include/SimpleRange.h
@@ -6,18 +6,18 @@
 
 namespace glow
 {
-    class OrderedRange : public Range
+    class SimpleRange : public Range
     {
     private:
         uint16_t begin;
         uint16_t end;
 
     public:
-        OrderedRange(uint16_t begin = 0, uint16_t end = 0) : begin(begin), end(end) {}
+        SimpleRange(uint16_t begin = 0, uint16_t end = 0) : begin(begin), end(end) {}
 
         inline void Begin(uint16_t v) { begin = v; }
         inline void End(uint16_t v) { end = v; }
-        
+
         virtual uint16_t Begin() { return begin; };
         virtual uint16_t End() { return end; }
     };
