0e559bc4f039317c7e28ace673a55d8824d6e84e
Dave Marsh <davemarsh77@gmail.com>
2022-06-30 08:42:21 -0400

    mapper indepentdent of range

--- include/Mapper.h
+++ include/Mapper.h
@@ -1,12 +1,12 @@
 // Copyright (c) 2022 Dave Marsh. See LICENSE.
 
 #pragma once
-#include "Range.h"
+#include "base.h"
 
 namespace glow
 {
     template <typename T>
-    class Mapper : public Range
+    class Mapper
     {
     public:
         virtual T &Get(uint16_t index) = 0;
