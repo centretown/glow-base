67be3bcf58d4bd3a0458dce942ef210e61202720
Dave Marsh <davemarsh77@gmail.com>
2022-06-29 22:58:54 -0400

    fixed OrderRange constructor

--- include/OrderedRange.h
+++ include/OrderedRange.h
@@ -13,7 +13,7 @@ namespace glow
         uint16_t end;
 
     public:
-        OrderedRange(uint16_t begin = 0, uint16_t end = 0) : begin(), end(end) {}
+        OrderedRange(uint16_t begin = 0, uint16_t end = 0) : begin(begin), end(end) {}
 
         inline void Begin(uint16_t v) { begin = v; }
         inline void End(uint16_t v) { end = v; }
