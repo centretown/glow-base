542da0bcbfc4b801fe2e2e6265b041cb22e7c402
Dave Marsh <davemarsh77@gmail.com>
2022-07-03 22:36:01 -0400

    added Length to Range

--- include/Range.h
+++ include/Range.h
@@ -14,6 +14,7 @@ namespace glow
     public:
         inline bool Reverse() { return reverse; }
         inline void Reverse(bool v) { reverse = v; }
+        inline const uint16_t Length() { return End() - Begin(); }
         virtual const uint16_t Begin() = 0;
         virtual const uint16_t End() = 0;
     };
