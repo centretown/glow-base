da4e11dc1a074130008f9d65e02de0c39bee153b
Dave Marsh <davemarsh77@gmail.com>
2022-07-13 17:05:48 -0400

    added tested Range operators

--- include/Range.h
+++ include/Range.h
@@ -13,6 +13,8 @@ namespace glow
         uint16_t end;
     } range_limits;
 
+    const uint16_t upper_limit = 0xffff;
+
     class Range
     {
     private:
@@ -54,6 +56,114 @@ namespace glow
             return *this;
         }
 
+        inline Range &operator=(range_pack other)
+        {
+            Pack(other);
+            return *this;
+        }
+
+        // move
+        inline Range &operator++()
+        {
+            if (limits.end < upper_limit)
+            {
+                ++limits.begin;
+                ++limits.end;
+                Resize(limits.begin, limits.end);
+            }
+            return *this;
+        }
+
+        inline Range &operator--()
+        {
+            if (limits.begin > 0)
+            {
+                --limits.begin;
+                --limits.end;
+                Resize(limits.begin, limits.end);
+            }
+            return *this;
+        }
+
+        inline Range &operator+=(uint16_t v)
+        {
+            uint16_t end = v + limits.end;
+            if (end > limits.end)
+            {
+                Resize(limits.begin + v, limits.end + v);
+            }
+            return *this;
+        }
+
+        inline Range &operator-=(uint16_t v)
+        {
+            if (v <= limits.begin)
+            {
+                Resize(Begin() - v, End() - v);
+            }
+            return *this;
+        }
+
+        inline range_pack operator+(uint16_t v)
+        {
+            uint16_t end = v + limits.end;
+            if (end < limits.end)
+            {
+                return Pack();
+            }
+            return Range(Begin() + v, End() + v).Pack();
+        }
+
+        inline range_pack operator-(uint16_t v)
+        {
+            if (v > limits.begin)
+            {
+                return Pack();
+            }
+            return Range(Begin() - v, End() - v).Pack();
+        }
+
+        // squeeze
+        inline Range &operator>>=(const uint16_t v)
+        {
+            uint16_t begin = v + Begin();
+            if (begin > Begin() && begin <= End())
+            {
+                Resize(begin, End());
+            }
+            return *this;
+        }
+
+        inline Range &operator<<=(const uint16_t v)
+        {
+            uint16_t end = End() - v;
+            if (end < End() && end >= Begin())
+            {
+                Resize(Begin(), end);
+            }
+            return *this;
+        }
+
+        inline range_pack operator<<(const uint16_t v)
+        {
+            uint16_t end = End() - v;
+            if (end < End() && end >= Begin())
+            {
+                return Range(Begin(), end).Pack();
+            }
+            return Pack();
+        }
+
+        inline range_pack operator>>(const uint16_t v)
+        {
+            uint16_t begin = v + Begin();
+            if (begin > Begin() && begin <= End())
+            {
+                return Range(begin, End()).Pack();
+            }
+            return Pack();
+        }
+
         inline void Copy(Range &range)
         {
             Pack(range.Pack());
@@ -79,6 +189,7 @@ namespace glow
             {
                 putter.Put(i, value);
             }
+            putter.Update();
         }
 
         template <typename PUTTER, typename MAPPER, typename VALUE>
@@ -88,24 +199,29 @@ namespace glow
             {
                 putter.Put(mapper.Map(i), value);
             }
+            putter.Update();
         }
 
         template <typename PUTTER, typename VALUE>
         inline void ReverseSpin(PUTTER &putter, VALUE value)
         {
-            for (uint16_t i = 1; i <= Length(); i++)
+            const uint16_t length = Length();
+            for (uint16_t i = 1; i <= length; i++)
             {
                 putter.Put(End() - i, value);
             }
+            putter.Update();
         }
 
         template <typename PUTTER, typename MAPPER, typename VALUE>
         inline void ReverseSpin(PUTTER &putter, MAPPER &mapper, VALUE value)
         {
-            for (uint16_t i = 1; i <= Length(); i++)
+            const uint16_t length = Length();
+            for (uint16_t i = 1; i <= length; i++)
             {
                 putter.Put(mapper.Map(End() - i), value);
             }
+            putter.Update();
         }
     };
 }
--- test/test_range_funcs.cpp
+++ test/test_range_funcs.cpp
@@ -37,6 +37,7 @@ typedef struct
         index = i;
         value = v;
     }
+    void Update() {}
 } TestPut;
 
 typedef struct
@@ -145,6 +146,224 @@ void testRangeOperators()
     TEST_ASSERT_EQUAL(90, target.End());
     TEST_ASSERT_EQUAL(50, source.Begin());
     TEST_ASSERT_EQUAL(90, source.End());
+
+    ++source;
+    TEST_ASSERT_EQUAL(51, source.Begin());
+    TEST_ASSERT_EQUAL(91, source.End());
+
+    ++source;
+    TEST_ASSERT_EQUAL(52, source.Begin());
+    TEST_ASSERT_EQUAL(92, source.End());
+
+    --source;
+    TEST_ASSERT_EQUAL(51, source.Begin());
+    TEST_ASSERT_EQUAL(91, source.End());
+
+    source = 0;
+    TEST_ASSERT_EQUAL(0, source.Begin());
+    TEST_ASSERT_EQUAL(0, source.End());
+
+    ++source;
+    TEST_ASSERT_EQUAL(1, source.Begin());
+    TEST_ASSERT_EQUAL(1, source.End());
+    TEST_ASSERT_EQUAL(0, source.Length());
+
+    source = Range(10, 100);
+    TEST_ASSERT_EQUAL(10, source.Begin());
+    TEST_ASSERT_EQUAL(100, source.End());
+
+    target = source + 1;
+    TEST_ASSERT_EQUAL(11, target.Begin());
+    TEST_ASSERT_EQUAL(101, target.End());
+
+    target = source + 5;
+    TEST_ASSERT_EQUAL(15, target.Begin());
+    TEST_ASSERT_EQUAL(105, target.End());
+
+    target = source - 5;
+    TEST_ASSERT_EQUAL(5, target.Begin());
+    TEST_ASSERT_EQUAL(95, target.End());
+
+    target += 5;
+    TEST_ASSERT_EQUAL(10, source.Begin());
+    TEST_ASSERT_EQUAL(100, source.End());
+
+    target -= 5;
+    TEST_ASSERT_EQUAL(5, target.Begin());
+    TEST_ASSERT_EQUAL(95, target.End());
+
+    target -= 5;
+    TEST_ASSERT_EQUAL(0, target.Begin());
+    TEST_ASSERT_EQUAL(90, target.End());
+
+    // begin less than zero not allowed no effect
+    target -= 5;
+    TEST_ASSERT_EQUAL(0, target.Begin());
+    TEST_ASSERT_EQUAL(90, target.End());
+
+    // begin less than zero not allowed no effect
+    --target;
+    TEST_ASSERT_EQUAL(0, target.Begin());
+    TEST_ASSERT_EQUAL(90, target.End());
+
+    // begin less than zero not allowed no effect
+    source = target - 1;
+    TEST_ASSERT_EQUAL(0, source.Begin());
+    TEST_ASSERT_EQUAL(90, source.End());
+
+    ++target;
+    TEST_ASSERT_EQUAL(1, target.Begin());
+    TEST_ASSERT_EQUAL(91, target.End());
+
+    source = Range(0xff00, 0xffff);
+    TEST_ASSERT_EQUAL_HEX(0xff00, source.Begin());
+    TEST_ASSERT_EQUAL_HEX(0xffff, source.End());
+
+    // end at max no effect
+    ++source;
+    TEST_ASSERT_EQUAL_HEX(0xff00, source.Begin());
+    TEST_ASSERT_EQUAL_HEX(0xffff, source.End());
+
+    // end at max no effect
+    source += 1;
+    TEST_ASSERT_EQUAL_HEX(0xff00, source.Begin());
+    TEST_ASSERT_EQUAL_HEX(0xffff, source.End());
+
+    // end at max no effect
+    target = source + 1;
+    TEST_ASSERT_EQUAL_HEX(0xff00, target.Begin());
+    TEST_ASSERT_EQUAL_HEX(0xffff, target.End());
+
+    source = Range(25, 50);
+    TEST_ASSERT_EQUAL(25, source.Begin());
+    TEST_ASSERT_EQUAL(50, source.End());
+
+    source >>= 1;
+    TEST_ASSERT_EQUAL(26, source.Begin());
+    TEST_ASSERT_EQUAL(50, source.End());
+
+    source >>= 2;
+    TEST_ASSERT_EQUAL(28, source.Begin());
+    TEST_ASSERT_EQUAL(50, source.End());
+
+    source >>= 20;
+    TEST_ASSERT_EQUAL(48, source.Begin());
+    TEST_ASSERT_EQUAL(50, source.End());
+
+    // no effect begin > end
+    source >>= 3;
+    TEST_ASSERT_EQUAL(48, source.Begin());
+    TEST_ASSERT_EQUAL(50, source.End());
+
+    // ok begin == end
+    source >>= 2;
+    TEST_ASSERT_EQUAL(50, source.Begin());
+    TEST_ASSERT_EQUAL(50, source.End());
+    TEST_ASSERT_EQUAL(0, source.Length());
+
+    source = Range(0xff00, 0xffff);
+    source >>= 0xff;
+    TEST_ASSERT_EQUAL(0xffff, source.Begin());
+    TEST_ASSERT_EQUAL(0xffff, source.End());
+
+    source.Resize(6, 50);
+    source <<= 1;
+    TEST_ASSERT_EQUAL(6, source.Begin());
+    TEST_ASSERT_EQUAL(49, source.End());
+
+    source <<= 42;
+    TEST_ASSERT_EQUAL(6, source.Begin());
+    TEST_ASSERT_EQUAL(7, source.End());
+
+    // no effect begin > end
+    source <<= 2;
+    TEST_ASSERT_EQUAL(6, source.Begin());
+    TEST_ASSERT_EQUAL(7, source.End());
+
+    // ok equal
+    source <<= 1;
+    TEST_ASSERT_EQUAL(6, source.Begin());
+    TEST_ASSERT_EQUAL(6, source.End());
+
+    // no effect end < begin
+    source <<= 1;
+    TEST_ASSERT_EQUAL(6, source.Begin());
+    TEST_ASSERT_EQUAL(6, source.End());
+
+    source.Resize(0, 5);
+    // no effect (new end) > (previous end)
+    source <<= 6;
+    TEST_ASSERT_EQUAL(0, source.Begin());
+    TEST_ASSERT_EQUAL(5, source.End());
+
+    source.Resize(50, 60);
+    target = source >> 1;
+    TEST_ASSERT_EQUAL(50, source.Begin());
+    TEST_ASSERT_EQUAL(60, source.End());
+    TEST_ASSERT_EQUAL(51, target.Begin());
+    TEST_ASSERT_EQUAL(60, target.End());
+
+    target = source >> 5;
+    TEST_ASSERT_EQUAL(55, target.Begin());
+    TEST_ASSERT_EQUAL(60, target.End());
+
+    target = source >> 10;
+    TEST_ASSERT_EQUAL(60, target.Begin());
+    TEST_ASSERT_EQUAL(60, target.End());
+
+    // no effect begin > end
+    target = source >> 11;
+    TEST_ASSERT_EQUAL(50, target.Begin());
+    TEST_ASSERT_EQUAL(60, target.End());
+    TEST_ASSERT_EQUAL(50, source.Begin());
+    TEST_ASSERT_EQUAL(60, source.End());
+
+    source.Resize(0xfff0, 0xffff);
+    target = source >> 0x0f;
+    TEST_ASSERT_EQUAL_HEX(0xffff, target.Begin());
+    TEST_ASSERT_EQUAL_HEX(0xffff, target.End());
+    TEST_ASSERT_EQUAL_HEX(0xfff0, source.Begin());
+    TEST_ASSERT_EQUAL_HEX(0xffff, source.End());
+
+    // no effect new begin < previous
+    target = source >> 0x10;
+    TEST_ASSERT_EQUAL(0xfff0, target.Begin());
+    TEST_ASSERT_EQUAL(0xffff, target.End());
+
+    source.Resize(0, 50);
+    TEST_ASSERT_EQUAL(0, source.Begin());
+    TEST_ASSERT_EQUAL(50, source.End());
+    target = source << 1;
+    TEST_ASSERT_EQUAL(0, target.Begin());
+    TEST_ASSERT_EQUAL(49, target.End());
+
+    target = source << 50;
+    TEST_ASSERT_EQUAL(0, target.Begin());
+    TEST_ASSERT_EQUAL(0, target.End());
+
+    // no effect end > previous
+    target = source << 51;
+    TEST_ASSERT_EQUAL(0, target.Begin());
+    TEST_ASSERT_EQUAL(50, target.End());
+
+    source.Resize(322, 111);
+    TEST_ASSERT_EQUAL(111, source.Begin());
+    TEST_ASSERT_EQUAL(322, source.End());
+    target = source << 200;
+    TEST_ASSERT_EQUAL(111, target.Begin());
+    TEST_ASSERT_EQUAL(122, target.End());
+
+    target = source << 211;
+    TEST_ASSERT_EQUAL(111, target.Begin());
+    TEST_ASSERT_EQUAL(111, target.End());
+
+    // no effect end < begin
+    target = source << 212;
+    TEST_ASSERT_EQUAL(111, source.Begin());
+    TEST_ASSERT_EQUAL(322, source.End());
+
+    TEST_ASSERT_EQUAL(111, target.Begin());
+    TEST_ASSERT_EQUAL(322, target.End());
 }
 
 void testRangeFuncs()
